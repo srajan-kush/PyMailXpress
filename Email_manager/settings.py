ef7963582b8349e2715f2ad2370aa58f0536be94
Srajan Kushwaha <144719261+srajan-kush@users.noreply.github.com>
2024-05-28 12:11:43 +0530

    Update settings.py

--- Email_manager/settings.py
+++ Email_manager/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ld*n2@l*@908s8cd@c0%5$m+4rmi9a=s+dnex!df_moiu@%-3u
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
